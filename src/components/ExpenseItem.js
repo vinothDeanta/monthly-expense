add07246305eda2b35e260379beb1b7f22891ffd
vinothDeanta <85175544+vinothDeanta@users.noreply.github.com>
2021-09-29 18:15:11 +0530

    Dynamic value pass from the parent component to child component using props

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,20 +1,25 @@
 import './ExpenseItem.css';
 
-function ExpenseItem() {
-    const expenseDate = new Date(2021, 3, 2);
-    const expenseTitle = 'Birthday Expene';
-    const expenseAmount = '$250';
+function ExpenseItem(props) {
     /*
     Notes:
      {} using the open and close braces we can execute the simple Javascript inside the HTML Content
-
+     
     */
+    const month = props.date.toLocaleString('en-US', { month:'long' });
+    const day = props.date.toLocaleString('en-US', { day:'2-digit' });
+    const year = props.date.getFullYear();
+
     return (
         <div className="expense-item">
-            <div>{ expenseDate.toISOString() }</div>
+            <div>
+                <div>{ month }</div>
+                <div>{ day }</div>
+                <div>{ year }</div>
+            </div>
             <div className="expense-item__description">
-                <h2>{ expenseTitle }</h2>
-                <div className="expense-item__price">{ expenseAmount }</div>
+                <h2>{ props.title }</h2>
+                <div className="expense-item__price">{  props.amount }</div>
             </div>
         </div>
     )
